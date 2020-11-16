a012c78ab0edeaed4c8fa1b866a7a25876ebfaae
Johannes Oster <johannes.oster@outlook.at>
2020-11-16 18:27:06 +0100

    Downgrade next to stable version and fix type error

--- src/lib/form-builder-utils.ts
+++ src/lib/form-builder-utils.ts
@@ -1,8 +1,6 @@
 import {ComponentType} from 'react';
 import {FormSectionHeader, RatingGroup} from 'components';
 import {Input, Select, Textarea, Radio, Checkbox} from 'icruiting-ui';
-import queryString from 'query-string';
-import {FormCategory} from 'services';
 
 /** Return the actual component to the passed components name */
 export const stringToComponent = (componentName: string): any => {
@@ -21,15 +19,4 @@ export const stringToComponent = (componentName: string): any => {
 
   if (!component) throw new Error('Invalid component string');
   return component;
-};
-
-/** extract form category from query string */
-export const getFormCategory = (query: string): FormCategory => {
-  const queryParam = queryString.parse(query).formCategory || '';
-  return queryParam as FormCategory;
-};
-
-export const getJobID = (query: string): string => {
-  const jobId = (queryString.parse(query).jobId || '') as string;
-  return jobId;
 };
