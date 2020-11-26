f247a43c321816b75296d32d56af829ba8598804
Johannes Oster <johannes.oster@outlook.at>
2020-11-26 15:33:14 +0100

    Move ReturnType of formBuilder hook to types file

--- src/lib/formbuilder/types.ts
+++ src/lib/formbuilder/types.ts
@@ -41,3 +41,35 @@ export interface FormBuilderQuery {
 export interface FormBuilderParams {
   jobId: string;
 }
+
+export interface ReturnType {
+  formFields: DnDItem[];
+
+  /** A function wich should be called if an existing item moves down or upwords.
+   * @params dragIndex  The initial index of the dragged item
+   * @params hoverIndex The index of the hovered item
+   */
+  moveItem: (dragIndex: number, hoverIndex: number) => void;
+  /** A function wich should be called if a new item moves over an existing one.
+   * @params item   The dragged item
+   * @params index  The index of the hovered item
+   */
+  addItem: (item: DnDItem, index: number) => void;
+  /** A function wich should be called if the delete button is pressed.
+   * The delete button will only be displayed if this function is provided.
+   * @params id The id of the item to delete */
+  deleteItem?: (id: string) => void;
+  /** A function wich should be called if the edit button is pressed.
+   * The edit button will only be displayed if this function is provided.
+   * @params id The id of the item to edit
+   * @params and object of key value pairs to update */
+  editItem?: (id: string, values: {[key: string]: string}) => void;
+  duplicateItem: (id: string) => void;
+  /** A function wich should be called if an items moves outside its target area. */
+  onOutsideHover: (item: DnDItem) => void;
+  /** A function wich should be called if an item is dropped.
+   * @returns number | null the id of the currentNewItemId if the droped item comes from the source, null otherwise
+   */
+  onDrop: () => number | null;
+  reset: (formFields: DnDItem[]) => void;
+}
--- src/lib/formbuilder/useFormBuilder.ts
+++ src/lib/formbuilder/useFormBuilder.ts
@@ -1,42 +1,9 @@
 import {useState, useRef, useCallback} from 'react';
-import {DnDItem} from './types';
+import {DnDItem, ReturnType} from './types';
 
-type Params = {
+interface Params {
   initialformFields?: DnDItem[];
-};
-
-type ReturnType = {
-  formFields: DnDItem[];
-
-  /** A function wich should be called if an existing item moves down or upwords.
-   * @params dragIndex  The initial index of the dragged item
-   * @params hoverIndex The index of the hovered item
-   */
-  moveItem: (dragIndex: number, hoverIndex: number) => void;
-  /** A function wich should be called if a new item moves over an existing one.
-   * @params item   The dragged item
-   * @params index  The index of the hovered item
-   */
-  addItem: (item: DnDItem, index: number) => void;
-  /** A function wich should be called if the delete button is pressed.
-   * The delete button will only be displayed if this function is provided.
-   * @params id The id of the item to delete */
-  deleteItem?: (id: string) => void;
-  /** A function wich should be called if the edit button is pressed.
-   * The edit button will only be displayed if this function is provided.
-   * @params id The id of the item to edit
-   * @params and object of key value pairs to update */
-  editItem?: (id: string, values: {[key: string]: string}) => void;
-  duplicateItem: (id: string) => void;
-  /** A function wich should be called if an items moves outside its target area. */
-  onOutsideHover: (item: DnDItem) => void;
-  /** A function wich should be called if an item is dropped.
-   * @returns number | null the id of the currentNewItemId if the droped item comes from the source, null otherwise
-   */
-  onDrop: () => number | null;
-  reset: (formFields: DnDItem[]) => void;
-};
-
+}
 export const useFormBuilder: (params: Params) => ReturnType = ({
   initialformFields = [],
 }) => {
