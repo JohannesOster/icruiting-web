3fc507e3dc669837c843874d1ca36000f2e85456
Johannes Oster <johannes.oster@outlook.at>
2021-02-23 21:04:18 +0100

    Remove standardDeviation from ranking converter

--- src/services/rankings/convert.ts
+++ src/services/rankings/convert.ts
@@ -3,9 +3,8 @@ import {TRanking, TRankingAPI} from './types';
 export const convertAPIRanking = (ranking: TRankingAPI): TRanking => {
   return (ranking.map((item) => {
     const score = parseFloat(item.score);
-    const standardDeviation = parseFloat(item.standardDeviation);
     const submissionsCount = parseInt(item.submissionsCount);
 
-    return {...item, score, standardDeviation, submissionsCount};
+    return {...item, score, submissionsCount};
   }) as unknown) as TRanking;
 };
