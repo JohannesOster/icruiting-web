c6ede021f4ebcb75ed3168f7adb25c36b93c4c63
Johannes Oster <johannes.oster@outlook.at>
2023-04-03 19:07:20 +0200

    Replace font @import with link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,6 @@ import 'styles/typography.css';
 Amplify.configure({...config, ssr: true});
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
   body {color: ${({theme}) => theme.colors.typographyPrimary}};
 `;
 
