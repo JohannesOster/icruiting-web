0f00d179de6b02e90046b40a2b78f2351ce18d6f
Johannes Oster <johannes.oster@outlook.at>
2023-04-06 16:25:04 +0200

    Add white background to Select component

--- src/components/Select/Select.sc.ts
+++ src/components/Select/Select.sc.ts
@@ -31,6 +31,7 @@ export const BaseSelect = styled.select`
   background: url("data:image/svg+xml;utf8,<svg height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z' fill='%23545454' /></svg>");
   background-repeat: no-repeat;
   background-position: right ${({theme}) => theme.spacing.scale200} top 50%;
+  background-color: ${({theme}) => theme.colors.surfaceDefault};
 `;
 
 export const Label = styled.label<LabelProps>`
