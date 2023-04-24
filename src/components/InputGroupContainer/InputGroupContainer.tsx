058ee97d55c17b7571afcdc24960a37a8b7d22f1
Johannes Oster <johannes.oster@outlook.at>
2023-04-24 13:41:33 +0200

    Remove Searchbox

--- src/components/InputGroupContainer/InputGroupContainer.tsx
+++ src/components/InputGroupContainer/InputGroupContainer.tsx
@@ -5,13 +5,13 @@ export const InputGroupContainer = styled.span`
   display: flex;
 `;
 
-export const getInputGroupContainerStyles = (Component: ReactNode) => `
-    ${InputGroupContainer} &:not(:first-child) ${Component} {
+export const getInputGroupContainerStyles = (Component?: ReactNode) => `
+    ${InputGroupContainer} &:not(:first-child) ${Component || ''} {
     border-top-left-radius: 0;
     border-bottom-left-radius: 0;
   }
 
-  ${InputGroupContainer} &:not(:last-child) ${Component} {
+  ${InputGroupContainer} &:not(:last-child) ${Component || ''} {
     border-top-right-radius: 0;
     border-bottom-right-radius: 0;
     margin-inline-end: -1px;
