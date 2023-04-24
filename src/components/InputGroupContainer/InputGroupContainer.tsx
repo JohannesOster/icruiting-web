a2ba6bdbc6c9701476ba39273fc8c73c524c8cf8
Johannes Oster <johannes.oster@outlook.at>
2023-04-24 13:34:02 +0200

    Update InputGroupContainer

--- src/components/InputGroupContainer/InputGroupContainer.tsx
+++ src/components/InputGroupContainer/InputGroupContainer.tsx
@@ -3,18 +3,6 @@ import styled from 'styled-components';
 
 export const InputGroupContainer = styled.span`
   display: flex;
-  flex-align: left;
-
-  & > *:not(:first-child) {
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-  }
-
-  & > *:not(:last-child) {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    margin-inline-end: -1px;
-  }
 `;
 
 export const getInputGroupContainerStyles = (Component: ReactNode) => `
