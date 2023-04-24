6e749d9047a8f38a3ee794b812d1b417ee5e69fe
Johannes Oster <johannes.oster@outlook.at>
2023-04-24 13:34:33 +0200

    Update AdvancedSelect

--- src/components/AdvancedSelect/AdvancedSelect.sc.ts
+++ src/components/AdvancedSelect/AdvancedSelect.sc.ts
@@ -7,7 +7,6 @@ export const ConentContainer = styled.div`
   display: flex;
   align-items: center;
   gap: ${({theme}) => theme.spacing.scale600};
-  width: 100%;
   padding: ${({theme}) => `${theme.spacing.scale200} ${theme.spacing.scale300}`};
 
   border: 1px solid ${({theme}) => theme.colors.inputBorder};
